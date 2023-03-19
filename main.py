82a92b349d7ca24cd042023d7db89b5f5acbbba4
akurjans <118132555+akurjans@users.noreply.github.com>
2023-03-20 01:35:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,49 +2,50 @@
 
 # python3
 
-import heapq
+# python3
 
+import heapq
 
 def parallel_processing(n, m, data):
     output = []
-    start_times = [0] * num_threads
-    thread_heap = [(0, i) for i in range(num_threads)]
-    
+    start_times= [0] * n
+    thread_heaps=[(0,i) for i in range (n)] 
 
-    for i in range(num_jobs):
-        processing_time = job_times[i]
+    for i in range(m):
+        processing_time=data[i]
 
-        free_time, thread_idx = heapq.heappop(thread_heap)
-        
+        free_time, thread_idx=heapq.heappop(thread_heaps)
+
         output.append((thread_idx, free_time))
 
-        start_times[thread_idx] = free_time + processing_time
+        start_times[thread_idx]=free_time+processing_time
 
-        heapq.heappush(thread_heap,(start_times[thread_idx], thread_idx))
+        heapq.heappush(thread_heaps,(start_times[thread_idx],thread_idx))
+
+
+   
+
     return output
 
+
 def main():
+   # n = 0
+   # m = 0
+
+    n,m=map(int, input().split())
+
     
-    num_threads, num_jobs = map(int, input().split())
+    data = [int(x) for x in input().split()]
 
-    job_times = [int(x) for x in input().split()]
-
-    result = parallel_processing(num_threads, num_jobs, job_times)
-
-    for thread_idx, start_time in result:
-        print(thread_idx, start_time)
-    
-
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
-
-    # TODO: create the function
     result = parallel_processing(n,m,data)
     
-    # TODO: print out the results, each pair in it's own line
+    
+
+    for a,start_time in result:
+        print(a,start_time)
 
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
+
