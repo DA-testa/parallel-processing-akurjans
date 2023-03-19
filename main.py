8ec3bd952946d56326a141b4ef543edea001e10a
akurjans <118132555+akurjans@users.noreply.github.com>
2023-03-20 01:39:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,35 @@
 # python3
 
-# python3
-
-# python3
-
-import heapq
-
-def parallel_processing(n, m, data):
+def parallel_processing(n_threads, n_jobs, job_times):
     output = []
-    start_times= [0] * n
-    thread_heaps=[(0,i) for i in range (n)] 
-
-    for i in range(m):
-        processing_time=data[i]
-
-        free_time, thread_idx=heapq.heappop(thread_heaps)
-
-        output.append((thread_idx, free_time))
-
-        start_times[thread_idx]=free_time+processing_time
-
-        heapq.heappush(thread_heaps,(start_times[thread_idx],thread_idx))
-
-
-   
-
+    # TODO: write the function for simulating parallel tasks, 
+    
+    thread_times = [0] * n_threads 
+    for i in range(n_jobs):
+        thread_idx = thread_times.index(min(thread_times))
+        output.append((thread_idx, thread_times[thread_idx]))
+        thread_times[thread_idx] += job_times[i]
     return output
 
-
 def main():
-   # n = 0
-   # m = 0
-
-    n,m=map(int, input().split())
-
-    
-    data = [int(x) for x in input().split()]
-
-    result = parallel_processing(n,m,data)
+    # TODO: create input from keyboard
     
     
+    # n_threads - thread count 
+    # n_jobs - job count
+    n_threads, n_jobs = map(int, input().split())
 
-    for a,start_time in result:
-        print(a,start_time)
+   
+    
+    job_times = list(map(int, input().split()))
 
+    # TODO: create the function
+    result = parallel_processing(n_threads, n_jobs, job_times)
+    
+    # TODO: print out the results, each pair in it's own line
+    for thread_idx, start_time in result:
+        print(thread_idx, start_time)
 
-
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
 
